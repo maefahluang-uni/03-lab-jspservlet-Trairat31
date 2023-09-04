c08d36894abcc76d72cf81ad7b5a9c170ae8022d
Trairat31 <122780011+Trairat31@users.noreply.github.com>
2023-09-04 09:02:17 +0000

    6531503031

--- src/main/java/th/mfu/BMICalculatorServlet.java
+++ src/main/java/th/mfu/BMICalculatorServlet.java
@@ -9,20 +9,39 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 //TODO: add webservlet to "/calbmi"
+@WebServlet("/calbmi")
 public class BMICalculatorServlet extends HttpServlet{
 
     @Override
     protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         //TODO: get parameter from request: "weight" and "height"
-        
+        double weight = Double.parseDouble(request.getParameter("weight"));
+        double height = Double.parseDouble(request.getParameter("height"));
         //TODO: calculate bmi
-
+        double bmi = calculateBMI(weight, height);
         //TODO: determine the built from BMI
-      
+        String bodyType = determineBodyType(bmi);
         //TODO: add bmi and built to the request's attribute
-
+        request.setAttribute("bmi", Math.round(bmi));
+        request.setAttribute("bodyType", bodyType);
         //TODO: forward to jsp
-           
+        request.getRequestDispatcher("/bmi_result.jsp").forward(request, response);
     }
-    
+    // Calculate BMI method
+    private double calculateBMI(double weight, double height) {
+        // BMI formula: BMI = weight(kg) / (height(m) * height(m))
+        return weight / (height * height);
+    }
+     // Determine body type method (a simple example)
+     private String determineBodyType(double bmi) {
+        if (bmi < 18.5) {
+            return "underweight";
+        } else if (bmi >= 18.5 && bmi < 24.9) {
+            return "normal weight";
+        } else if (bmi >= 24.9 && bmi < 29.9) {
+            return "overweight";
+        } else {
+            return "obese";
+        }
+    }
 }
--- src/main/webapp/bmi_result.jsp
+++ src/main/webapp/bmi_result.jsp
@@ -3,11 +3,11 @@
    <head>
       <title>BMI Calculator</title>
    </head>
-<body>
-<img src="https://www.cdc.gov/healthyweight/images/assessing/bmi-adult-fb-600x315.jpg?_=07167">
-<!--TODO: show bmi below -->
-<h2>Result is <%= %></h2>
-<!--TODO: show built type below -->
-<h3>According to your BMI, you are considered <%= %></h3>
-</body>
+   <body>
+      <img src="https://www.cdc.gov/healthyweight/images/assessing/bmi-adult-fb-600x315.jpg?_=07167">
+      <!-- Display BMI -->
+      <h2>Result is <%= request.getAttribute("bmi") %></h2>
+      <!-- Display body type -->
+      <h3>According to your BMI, you are considered <%= request.getAttribute("bodyType") %></h3>
+   </body>
 </html>
